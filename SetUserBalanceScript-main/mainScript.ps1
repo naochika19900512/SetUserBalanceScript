90a10ec85d2c8a7bd92391d7b3dbda5e27d5ccdf
naochika19900512 <125625688+naochika19900512@users.noreply.github.com>
2023-03-30 16:30:28 +0900

    管理者権限で実行した時にワークフォルダが変わるバグを修正

--- SetUserBalanceScript-main/mainScript.ps1
+++ SetUserBalanceScript-main/mainScript.ps1
@@ -1,4 +1,9 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+        Exit;
+    }
+}
 $XML = [XML](Get-Content -Encoding default .\config.xml)
 $servercommand = $XML.root.ConfigList.ServerCommandExePath
 $accountList = $XML.root.ConfigList.AccountList.Split(",")
